5fce39f54d7b5f185cadc097f07be88eefe92266
ry <yeryanyin@gmail.com>
2026-03-10 09:22:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 import json
+
 ## Open the JSON file of pokemon data
 pokedex = open("./pokedex.json", encoding="utf8")
+
+
+
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
-""" print(data[0]) """
+"print(data[0])"
 
-import random
+
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
-for i in data:
-    print(data[i]['name']['english'])
-    
+"""for i in range(809):
+    print(data[i]['name']['english'])"""
+
+
+
 # Add a language choice feature and print the pokemons name based on the user input
+language_finnish = input('Select language')
+
+for i in data:
+    if language_finnish == "english":
+        print(data[i]['name']['english'])
+    if language_finnish == 'japanese':
+        print(data[i]['name']['japanese'])
+    if language_finnish == "chinese":
+        print(data[i]['name']['chinese'])
+    if language_finnish == "french":
+        print(data[i]['name']['french'])
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
 
+
+
+
 # Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
+
+
+
