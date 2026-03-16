6e9b19c9e9bb8815c8a233e2a352b1b835e0a154
ry <yeryanyin@gmail.com>
2026-03-16 09:25:33 -0400

    el toro better than houdini mansion

--- app.py
+++ app.py
@@ -18,14 +18,17 @@ data = json.load(pokedex)
 
 
 # Add a language choice feature and print the pokemons name based on the user input
-language_finnish = input('Select language:\n')
+"""language_finnish = input('Select language:\n')
 
 for i in data:
-    print(i['name'][language_finnish])
+    print(i['name'][language_finnish])"""
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
+type_input = input('Select a pokemon type\n')
 
-
+for i in data:
+    if type_input in i['type']:
+        print(i)
 
 
 # Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
